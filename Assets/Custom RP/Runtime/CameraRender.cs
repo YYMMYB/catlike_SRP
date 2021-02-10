93bad758211329de527c01e283a2d2ab3770fe5f
yymmyb <1329112803@qq.com>
2021-02-10 23:07:01 +0800

    2.4 清除渲染目标

--- Assets/Custom RP/Runtime/CameraRender.cs
+++ Assets/Custom RP/Runtime/CameraRender.cs
@@ -27,9 +27,10 @@ public class CameraRenderer
 
     void Setup()
     {
+        context.SetupCameraProperties(camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
         buffer.BeginSample(bufferName);
         ExecuteBuffer();
-        context.SetupCameraProperties(camera);
     }
 
     void Submit()
