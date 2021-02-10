4f24102c9c5f7ab8769953b3ca092ad3b3f70692
yymmyb <1329112803@qq.com>
2021-02-10 23:37:43 +0800

    3.3 部分类

--- Assets/Custom RP/Runtime/CameraRender.cs
+++ Assets/Custom RP/Runtime/CameraRender.cs
@@ -1,21 +1,10 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering;
 
-public class CameraRenderer
+public partial class CameraRenderer
 {
 
     static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
-
-    static ShaderTagId[] legacyShaderTagIds = {
-        new ShaderTagId("Always"),
-        new ShaderTagId("ForwardBase"),
-        new ShaderTagId("PrepassBase"),
-        new ShaderTagId("Vertex"),
-        new ShaderTagId("VertexLMRGBM"),
-        new ShaderTagId("VertexLM")
-    };
-
-    static Material errorMaterial;
 
     ScriptableRenderContext context;
 
@@ -102,26 +91,5 @@ public class CameraRenderer
         );
     }
 
-    void DrawUnsupportedShaders()
-    {
-        if (errorMaterial == null)
-        {
-            errorMaterial =
-                new Material(Shader.Find("Hidden/InternalErrorShader"));
-        }
-
-        var drawingSettings = new DrawingSettings(
-            legacyShaderTagIds[0], new SortingSettings(camera)
-        ) {
-            overrideMaterial = errorMaterial
-        };
-        for (int i = 1; i < legacyShaderTagIds.Length; i++)
-        {
-            drawingSettings.SetShaderPassName(i, legacyShaderTagIds[i]);
-        }
-        var filteringSettings = FilteringSettings.defaultValue;
-        context.DrawRenderers(
-            cullingResults, ref drawingSettings, ref filteringSettings
-        );
-    }
+    partial void DrawUnsupportedShaders();
 }
