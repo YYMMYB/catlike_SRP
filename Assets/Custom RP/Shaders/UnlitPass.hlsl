c5870943fa2c486ba40103cfe4aff2566a65bd5e
yymmyb <1329112803@qq.com>
2021-02-11 20:46:42 +0800

    1.4 Shader 函数

--- Assets/Custom RP/Shaders/UnlitPass.hlsl
+++ Assets/Custom RP/Shaders/UnlitPass.hlsl
@@ -1,4 +1,12 @@
 ﻿#ifndef CUSTOM_UNLIT_PASS_INCLUDED
 #define CUSTOM_UNLIT_PASS_INCLUDED
 
+float4 UnlitPassVertex () : SV_POSITION {
+	return 0.0;
+}
+
+float4 UnlitPassFragment () : SV_TARGET {
+	return 0.0;
+}
+
 #endif
