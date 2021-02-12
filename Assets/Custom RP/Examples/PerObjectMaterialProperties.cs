aa8268afe326f6c4abe6fcfdfcca03cfb137b9af
yymmyb <1329112803@qq.com>
2021-02-12 10:29:23 +0800

    3.3 Surface 属性

--- Assets/Custom RP/Examples/PerObjectMaterialProperties.cs
+++ Assets/Custom RP/Examples/PerObjectMaterialProperties.cs
@@ -3,15 +3,18 @@
 [DisallowMultipleComponent]
 public class PerObjectMaterialProperties : MonoBehaviour
 {
-    static int baseColorId = Shader.PropertyToID("_BaseColor");
-    static int cutoffId = Shader.PropertyToID("_Cutoff");
+    static int
+        baseColorId = Shader.PropertyToID("_BaseColor"),
+        cutoffId = Shader.PropertyToID("_Cutoff"),
+        metallicId = Shader.PropertyToID("_Metallic"),
+        smoothnessId = Shader.PropertyToID("_Smoothness");
 
     static MaterialPropertyBlock block;
 
     [SerializeField]
     Color baseColor = Color.white;
     [SerializeField, Range(0f, 1f)]
-    float cutoff = 0.5f;
+    float alphaCutoff = 0.5f, metallic = 0f, smoothness = 0.5f;
 
     void OnValidate()
     {
@@ -20,7 +23,9 @@ public class PerObjectMaterialProperties : MonoBehaviour
             block = new MaterialPropertyBlock();
         }
         block.SetColor(baseColorId, baseColor);
-        block.SetFloat(cutoffId, cutoff);
+        block.SetFloat(cutoffId, alphaCutoff);
+        block.SetFloat(metallicId, metallic);
+        block.SetFloat(smoothnessId, smoothness);
         GetComponent<Renderer>().SetPropertyBlock(block);
     }
 
