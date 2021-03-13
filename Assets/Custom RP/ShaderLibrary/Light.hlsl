4ac75e772e07cfeadb0e330d937d85911fbbf92c
yymmyb <1329112803@qq.com>
2021-03-13 16:00:54 +0800

    2.5 减弱灯光

--- Assets/Custom RP/ShaderLibrary/Light.hlsl
+++ Assets/Custom RP/ShaderLibrary/Light.hlsl
@@ -13,18 +13,28 @@ CBUFFER_END
 struct Light {
     float3 color;
     float3 direction;
+    float attenuation;
 };
 
 int GetDirectionalLightCount () {
 	return _DirectionalLightCount;
 }
 
+DirectionalShadowData GetDirectionalShadowData (int lightIndex) {
+	DirectionalShadowData data;
+	data.strength = _DirectionalLightShadowData[lightIndex].x;
+	data.tileIndex = _DirectionalLightShadowData[lightIndex].y;
+	return data;
+}
 
-Light GetDirectionalLight (int index) {
+Light GetDirectionalLight (int index, Surface surfaceWS) {
     Light light;
     light.color = _DirectionalLightColors[index].rgb;
     light.direction = _DirectionalLightDirections[index].xyz;
+    DirectionalShadowData shadowData = GetDirectionalShadowData(index);
+	light.attenuation = GetDirectionalShadowAttenuation(shadowData, surfaceWS);
     return light;
 }
+
 
 #endif
